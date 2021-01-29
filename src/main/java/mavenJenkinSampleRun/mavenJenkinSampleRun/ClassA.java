ece33726702645cb6499b2aec2b262c0c32e4d50
schittireddy <sudheerqa.selenium@gmail.com>
2021-01-29 17:44:21 -0600

    sending or uploading all project to git

---
+++ src/main/java/mavenJenkinSampleRun/mavenJenkinSampleRun/ClassA.java
@@ -0,0 +1,50 @@
+package mavenJenkinSampleRun.mavenJenkinSampleRun;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	
+	@Test
+	public void aa() {
+		System.out.println("Hello World Runing from jenkins");
+	}
+	@Test
+	public void bb() {
+		System.out.println("Hello World Runing from jenkins");
+		
+	}
+	@Test
+	public void cc() {
+		System.out.println("Hello World Runing from jenkins");
+		
+	}
+	@Test
+	public void dd() {
+		System.out.println("Hello World Runing from jenkins");
+		
+	}
+	@Test
+	public void ee() {
+		System.out.println("Hello World Runing from jenkins");
+		
+	}
+	@Test
+	public void ff() {
+		System.out.println("Hello World Runing from jenkins");
+		
+	}
+	@Test
+	public void gg() {
+		System.out.println("Hello World Runing from jenkins");
+	}
+	@Test
+	public void hh() {
+		System.out.println("Hello World Runing from jenkins");
+	}
+	@Test
+	public void kk() {
+		System.out.println("Hello World Runing from jenkins");
+		
+	}
+
+}
